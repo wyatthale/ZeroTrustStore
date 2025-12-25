58bb8450a2cabb3a28f8a3b213559bccb62f2c47
wyatthale <ZoglmannFuria832@hotmail.com>
2025-12-25 11:46:04 +0800

    Update deployment script for ZeroTrustStore

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedZeroTrustStore = await deploy("ZeroTrustStore", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`ZeroTrustStore contract: `, deployedZeroTrustStore.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_zeroTrustStore"; // id required to prevent reexecution
+func.tags = ["ZeroTrustStore"];
